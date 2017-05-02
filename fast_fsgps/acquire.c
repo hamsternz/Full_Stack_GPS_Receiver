64e3f8d00f04b1bdac3bccc43b6d46ab48f90026
Mike Field <hamster@snap.net.nz>
2017-05-03 08:17:43 +1200

    Start on code to acquire

---
+++ fast_fsgps/acquire.c
@@ -0,0 +1,4 @@
+typedef unsigned int uint_32;
+void acquire_setup(void);
+int acquire_start(int sv_id, void (*success_callback)(int sv, uint_32 freq, uint_32 offset), void (*failed_callback)(int sv));
+int acquire_stop(int sv_id);
